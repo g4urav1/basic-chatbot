55a23394942d9965c2a84af4349611cc015f9e3b
g4urav1 <gauravkumarjha2107@gmail.com>
2026-06-29 10:34:32 +0530

    added streaming response

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -5,6 +5,7 @@ import LoginNav from "../components/loginnav";
 import LoginSidebar from "../components/loginsidebar";
 import LogoutSidebar from "../components/logoutsidebar";
 import LoginPage from "../components/loginpage";
+import ReactMarkown from "react-markdown";
 import { LoginBoxContext, LoginContext, MessagesContext, MobileContext, SidebarContext } from "../context/context";
 
 
@@ -15,26 +16,26 @@ export default function Home() {
   const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext);
   const { setIsMobile } = useContext(MobileContext);
   const { sidebarOpen, setSidebarOpen } = useContext(SidebarContext);
-  const {messages, setMessages} = useContext(MessagesContext);
+  const { messages, setMessages } = useContext(MessagesContext);
   const [message, setMessage] = useState("");
   const [isTyping, setIsTyping] = useState(false);
   const hasMessage = message.trim().length > 0;
 
+  const [streamingRespoonse, setStreamingResponse] = useState("");
+  const [streaming, setStreaming] = useState(false);
 
   const handleSubmit = async () => {
-    if (!message.trim()) return;
-
+    if (!message.trim()) return
     const userMessage = message;
-
     setMessages((prev) => [
       ...prev,
       { role: "user", content: userMessage }
     ]);
-
-    setMessage("");
+    setMessage("")
     setIsTyping(true);
-
     try {
+      setStreaming(true);
+      setStreamingResponse("");
       const response = await fetch("http://localhost:1111/chat", {
         method: "POST",
         headers: {
@@ -44,21 +45,32 @@ export default function Home() {
           message: userMessage,
         }),
       });
+      const reader = response.body.getReader();
+      const decoder = new TextDecoder();
+      let genResponse = "";
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
 
-      const data = await response.json();
+          setMessages((prev) => [
+            ...prev,
+            { role: "assistant", content: genResponse }
+          ]);
 
-      setMessages((prev) => [
-        ...prev,
-        { role: "assistant", content: data.content }
-      ]);
+          break;
+        };
+        genResponse += decoder.decode(value);
+        setStreamingResponse(genResponse);
+      }
     } catch (error) {
-       setMessages((prev) => [
+      setMessages((prev) => [
         ...prev,
         { role: "assistant", content: "🟥Server Error" }
       ])
       console.error(error);
     } finally {
       setIsTyping(false);
+      setStreaming(false);
     }
   };
 
@@ -88,17 +100,18 @@ export default function Home() {
 
   return (
 
-    <div className="flex h-screen bg-black text-white font-sans scrollbar-dark overflow-hidden">
+    <div className="flex h-screen text-white font-sans scrollbar-dark overflow-hidden">
 
       {showLogin && <LoginPage />}
 
       {isLoggedIn ? <LoginSidebar /> : <LogoutSidebar />}
 
-      <div className="flex-1 flex flex-col h-screen overflow-hidden bg-[#000000]">
+      <div className="flex-1 flex flex-col h-screen overflow-hidden bg-black">
 
 
 
         {isLoggedIn ? <LoginNav /> : <LogoutNav />}
+
 
 
         <div className="flex-1 overflow-y-auto px-4 py-6">
@@ -113,21 +126,37 @@ export default function Home() {
               {messages.map((msg, index) => (
                 <div
                   key={index}
-                  className={`p-3 px-4 rounded-full max-w-[80%] ${msg.role === "user"
-                    ? "bg-[#303030] self-end"
-                    : "bg-transparent self-start"
+                  className={`prose dark:prose-invert text-white p-3 px-4 rounded-full max-w-[80%]
+                   ${msg.role === "user"
+                      ? "bg-[#2B2B2B] self-end"
+                      : "bg-transparent self-start"
                     }`}
                 >
-                  {msg.content}
+                  <ReactMarkown >
+                    {msg.content}
+                  </ReactMarkown>
                 </div>
               ))}
 
+
               {isTyping && (
-                <div className="self-start px-4 py-3 rounded-2xl animate-pulse"
+                <div className="self-start px-4 pt-3 rounded-2xl animate-pulse" 
                   style={{ animationDelay: "400ms" }} >
                   Thinking...
                 </div>
               )}
+
+
+              {streaming && <div
+
+                className={`prose dark:prose-invert text-white p-3 px-4 
+  rounded-full max-w-[80%] bg-transparent self-start`}
+              >
+                <ReactMarkown >
+                  {streamingRespoonse}
+                </ReactMarkown>
+              </div>}
+
             </div>
           )}
         </div>
